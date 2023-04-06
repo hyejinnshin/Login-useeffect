6420002175b04121b44b6c30a594870271e50b41
hyejin.shin <hyejin.shin@asurion.com>
2023-04-06 20:22:21 +0900

    context

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -6,8 +6,8 @@ import classes from './MainHeader.module.css';
 const MainHeader = (props) => {
   return (
     <header className={classes['main-header']}>
-      <h1>useEffect</h1>
-      <Navigation isLoggedIn={props.isAuthenticated} onLogout={props.onLogout} />
+      <h1>A Typical Page</h1>
+      <Navigation />
     </header>
   );
 };
